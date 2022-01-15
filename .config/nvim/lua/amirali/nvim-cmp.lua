8b7678740d0428c158ba0676cad643e2d1fba128
Amirali Esfandiari <amiralinull@gmail.com>
2022-01-15 04:19:15 +0330

    fix neovim autocompletion

--- .config/nvim/lua/amirali/nvim-cmp.lua
+++ .config/nvim/lua/amirali/nvim-cmp.lua
@@ -4,7 +4,7 @@ local luasnip = require('luasnip')
 cmp.setup {
 	snippet = {
 		expand = function(args)
-			luasnip.lsp_epand(args.body)
+			luasnip.lsp_expand(args.body)
 		end
 	},
 	completion = {
