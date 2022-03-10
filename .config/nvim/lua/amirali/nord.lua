a347647c2b52a672a70f782ee05facbff9930b9e
Amirali Esfandiari <amiralinull@gmail.com>
2022-03-10 08:37:16 +0330

    lazy load nord theme just if it exists

--- .config/nvim/lua/amirali/nord.lua
+++ .config/nvim/lua/amirali/nord.lua
@@ -4,4 +4,7 @@ vim.g.nord_borders = false
 vim.g.nord_disable_background = false
 
 -- require('nord').set()
-vim.cmd [[colo nord]]
+local has_nord, _ = pcall(require, 'nord')
+if has_nord then
+  vim.cmd [[colo nord]]
+end
