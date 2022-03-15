7c73086f71e990cdcf8848e7bcbddb69251da134
Amirali Esfandiari <amiralinull@gmail.com>
2022-03-15 06:58:10 +0330

    2022-03-15

--- .config/nvim/lua/amirali/indent-blankline.lua
+++ .config/nvim/lua/amirali/indent-blankline.lua
@@ -1,5 +1,7 @@
 require('indent_blankline').setup {
   show_first_indent_level = false,
+  show_current_context = true,
+  show_current_context_start = true,
   buftype_exclude = {
     'terminal',
     'nofile'
