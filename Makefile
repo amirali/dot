cf4e5779ad8c35e90c6a00ead87a8a371605b10a
Amirali Esfandiari <amiralinull@gmail.com>
2022-03-30 11:35:29 +0430

    new structure with gnu stow

--- Makefile
+++ Makefile
@@ -5,20 +5,18 @@ OS = $(shell awk '/^ID=/' /etc/os-release | sed 's/ID=//' | sed 's/[.]0/./')
 base:
 ifeq ($(OS),debian)
 	sudo apt update
-	sudo apt install neovim golang kitty cmake make python3-dev python3-pip ripgrep zsh curl -y
+	sudo apt install neovim golang kitty cmake make python3-dev python3-pip ripgrep zsh curl stow -y
 else ifeq ($(OS),fedora) # Fuck you dear Fedora
-	sudo dnf install zsh vim kitty tmux cmake gcc-c++ make python3-devel golang fzf the_silver_searcher ripgrep yarn curl -y
-else ifeq ($(OS),opensuse-tumbleweed)
-	sudo zypper in zsh vim kitty tmux cmake gcc-c++ python3-devel go fzf the_silver_searcher ripgrep yarn curl -y
+	sudo dnf install zsh vim kitty tmux cmake gcc-c++ make python3-devel golang fzf ripgrep yarn curl stow -y
 else ifeq ($(OS),arch)
-	sudo pacman -S zsh vim kitty tmux cmake go fzf the_silver_searcher ripgrep yarn curl --noconfirm
+	sudo pacman -S zsh vim kitty tmux cmake go fzf ripgrep yarn curl stow --noconfirm
 else
 	@echo OH NO! We don't do that here :))
 	exit 0
 endif
 	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 	git submodule update --init
-	cp -r .config .oh-my-zsh .fonts .zshrc ~/
+	stow --verbose */
 	nvim +PackerSync
 
 lsp:
@@ -36,15 +34,5 @@ rust:
 
 wasm-pack: rust
 	curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh
-
-i3:
-ifeq ($(OS),fedora)
-	sudo dnf install i3-gaps i3status i3lock feh -y
-else ifeq ($(OS),arch)
-	sudo pacman -S i3-gaps i3status i3lock feh --noconfirm
-else
-	@echo OH NO! We don't do that here :))
-	exit 0
-endif
 
 all: base lsp rust wasm-pack
---
+++ nvim/.config/nvim/lua/settings.lua
@@ -0,0 +1,117 @@
+local cmd = vim.cmd     	-- execute Vim commands
+local exec = vim.api.nvim_exec 	-- execute Vimscript
+local fn = vim.fn       	-- call Vim functions
+local g = vim.g         	-- global variables
+local opt = vim.opt         	-- global/buffer/windows-scoped options
+
+opt.mouse = 'a'
+opt.clipboard = 'unnamedplus'
+opt.swapfile = false
+
+opt.number = true
+opt.relativenumber = true
+opt.foldmethod = 'expr'
+cmd [[set foldexpr=nvim_treesitter#foldexpr()]]
+opt.colorcolumn = '100'
+opt.splitright = true
+opt.splitbelow = true
+opt.ignorecase = true
+opt.smartcase = true
+opt.linebreak = true
+
+cmd [[au BufWritePre * :%s/\s\+$//e]]
+
+exec([[
+  augroup YankHighlight
+    autocmd!
+    autocmd TextYankPost * silent! lua vim.highlight.on_yank{higroup="IncSearch", timeout=700}
+  augroup end
+]], false)
+
+opt.hidden = true
+opt.history = 100
+opt.lazyredraw = true
+opt.synmaxcol = 240
+
+opt.termguicolors = true
+
+opt.expandtab = true
+opt.shiftwidth = 4
+opt.tabstop = 4
+opt.smartindent = true
+
+cmd [[au BufEnter * set fo-=c fo-=r fo-=o]]
+
+cmd [[
+  autocmd FileType lua,yaml setlocal shiftwidth=2 tabstop=2
+]]
+
+-- set yaml.ansible filetype (https://github.com/pearofducks/ansible-vim/blob/master/ftdetect/ansible.vim)
+cmd [[
+  function! s:isAnsible()
+    let filepath = expand("%:p")
+    let filename = expand("%:t")
+    if filepath =~ '\v/(tasks|roles|handlers)/.*\.ya?ml$' | return 1 | en
+    if filepath =~ '\v/(group|host)_vars/' | return 1 | en
+    let s:ftdetect_filename_regex = '\v(playbook|site|main|local|requirements)\.ya?ml$'
+    if exists("g:ansible_ftdetect_filename_regex")
+      let s:ftdetect_filename_regex = g:ansible_ftdetect_filename_regex
+    endif
+
+    if filename =~ s:ftdetect_filename_regex | return 1 | en
+
+    let shebang = getline(1)
+    if shebang =~# '^#!.*/bin/env\s\+ansible-playbook\>' | return 1 | en
+    if shebang =~# '^#!.*/bin/ansible-playbook\>' | return 1 | en
+
+    return 0
+  endfunction
+
+  augroup ansible_vim_ftyaml_ansible
+    au!
+    au BufNewFile,BufRead * if s:isAnsible() | set ft=yaml.ansible | en
+  augroup END
+  augroup ansible_vim_fthosts
+    au!
+    au BufNewFile,BufRead hosts set ft=ansible_hosts
+  augroup END
+]]
+
+cmd [[
+  autocmd FileType go setlocal noexpandtab
+]]
+
+opt.completeopt = 'menuone,noselect'
+
+cmd [[command Term :botright vsplit term://$SHELL]]
+
+cmd [[
+    autocmd TermOpen * setlocal listchars= nonumber norelativenumber nocursorline
+    autocmd TermOpen * startinsert
+    autocmd BufLeave term://* stopinsert
+]]
+
+local disabled_built_ins = {
+    "netrw",
+    "netrwPlugin",
+    "netrwSettings",
+    "netrwFileHandlers",
+    "gzip",
+    "zip",
+    "zipPlugin",
+    "tar",
+    "tarPlugin",
+    "getscript",
+    "getscriptPlugin",
+    "vimball",
+    "vimballPlugin",
+    "2html_plugin",
+    "logipat",
+    "rrhelper",
+    "spellfile_plugin",
+    "matchit"
+}
+
+for _, plugin in pairs(disabled_built_ins) do
+    g["loaded_" .. plugin] = 1
+end
